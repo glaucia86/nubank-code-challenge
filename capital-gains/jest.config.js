4ef578402cf8ef9f7ee922717df08fb4e74dd20a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2025-06-17 23:25:22 -0300

    chore: add Jest configuration for testing with TypeScript

---
+++ capital-gains/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
+};
